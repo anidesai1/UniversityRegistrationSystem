5e6082eb209569d2db2391fae541ac90126cabbf
iamedddie <edyolisa@gmail.com>
2024-01-16 15:36:53 +0000

    moved reg back to schema

--- src/main/resources/data/registration.sql
+++ src/main/resources/data/registration.sql
@@ -1,18 +1,3 @@
-DROP TABLE IF EXISTS REGISTRATION;
-CREATE TABLE REGISTRATION (
-                              registration_id INT AUTO_INCREMENT PRIMARY KEY,
-                              student_id INT,
-                              offering_id INT,
-                              registration_date DATE,
-                              grade VARCHAR(1),
-                              CHECK (grade IN ('A', 'B', 'C', 'D', 'E','F')),
-                              class_rating INT,
-                              CHECK (class_rating <=5)
-
-);
-
-
-
 INSERT INTO REGISTRATION (student_id, offering_id, registration_date, grade, class_rating)
 VALUES
     (1, 101, '2023-01-15', 'A', 4),
