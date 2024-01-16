e1c074a3c05e17cb1e6c6f82d8dff050aa7b802e
shonanohs <shonafitzpatrick15@gmail.com>
2024-01-16 15:48:35 +0000

    Removed ids from create student table

--- src/main/resources/data/student.sql
+++ src/main/resources/data/student.sql
@@ -1,12 +1,12 @@
-INSERT INTO STUDENT (student_id, first_name, last_name, email, major, student_year, last_registration_date)
+INSERT INTO STUDENT (first_name, last_name, email, major, student_year, last_registration_date)
     VALUES
-        (1, 'John', 'Doe', 'john.doe@example.com', 'Computer Science', 'Sophomore', '2023-05-15'),
-        (2, 'Jane', 'Smith', 'jane.smith@example.com', 'Art and Design', 'Junior', '2023-04-20'),
-        (3, 'Alex', 'Johnson', 'alex.johnson@example.com', 'Business Administration', 'Freshman', '2023-09-10'),
-        (4, 'Emily', 'Brown', 'emily.brown@example.com', 'Psychology', 'Senior', '2022-12-01'),
-        (5, 'Michael', 'Taylor', 'michael.taylor@example.com', 'Business Administration', 'Sophomore', '2023-03-18'),
-        (6, 'Olivia', 'Clark', 'olivia.clark@example.com', 'Psychology', 'Junior', '2023-07-25'),
-        (7, 'Daniel', 'Wang', 'daniel.wang@example.com', 'Computer Science', 'Senior', '2022-11-05'),
-        (8, 'Sophia', 'Garcia', 'sophia.garcia@example.com', 'Art and Design', 'Freshman', '2023-08-30'),
-        (9, 'Ethan', 'Lopez', 'ethan.lopez@example.com', 'Computer Science', 'Junior', '2023-02-14'),
-        (10, 'Ava', 'Martinez', 'ava.martinez@example.com', 'Art and Design', 'Senior', '2022-10-12');
+        ('John', 'Doe', 'john.doe@example.com', 'Computer Science', 'Sophomore', '2023-05-15'),
+        ('Jane', 'Smith', 'jane.smith@example.com', 'Art and Design', 'Junior', '2023-04-20'),
+        ('Alex', 'Johnson', 'alex.johnson@example.com', 'Business Administration', 'Freshman', '2023-09-10'),
+        ('Emily', 'Brown', 'emily.brown@example.com', 'Psychology', 'Senior', '2022-12-01'),
+        ('Michael', 'Taylor', 'michael.taylor@example.com', 'Business Administration', 'Sophomore', '2023-03-18'),
+        ('Olivia', 'Clark', 'olivia.clark@example.com', 'Psychology', 'Junior', '2023-07-25'),
+        ('Daniel', 'Wang', 'daniel.wang@example.com', 'Computer Science', 'Senior', '2022-11-05'),
+        ('Sophia', 'Garcia', 'sophia.garcia@example.com', 'Art and Design', 'Freshman', '2023-08-30'),
+        ('Ethan', 'Lopez', 'ethan.lopez@example.com', 'Computer Science', 'Junior', '2023-02-14'),
+        ('Ava', 'Martinez', 'ava.martinez@example.com', 'Art and Design', 'Senior', '2022-10-12');
