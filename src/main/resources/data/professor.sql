58cc951f770b0c87f94e8999092d56fedd000bc9
Grace <gahogan2000@gmail.com>
2024-01-16 15:43:32 +0000

    Changed data input to match

--- src/main/resources/data/professor.sql
+++ src/main/resources/data/professor.sql
@@ -1,6 +1,6 @@
 INSERT INTO PROFESSOR (first_name, last_name, title, department) VALUES
 ('John', 'Smith', 'Professor', 'Computer Science'),
-('Alice', 'Johnson', 'Associate Professor', 'Mathematics'),
-('Michael', 'Brown', 'Assistant Professor', 'Physics'),
-('Emily', 'Davis', 'Professor', 'Chemistry'),
-('Robert', 'Miller', 'Associate Professor', 'Biology');
+('Alice', 'Johnson', 'Associate Professor', 'Art and Design'),
+('Michael', 'Brown', 'Assistant Professor', 'Business Administration'),
+('Emily', 'Davis', 'Professor', 'Psychology'),
+('Robert', 'Miller', 'Associate Professor', 'Computer Science');
--- src/main/resources/data/student.sql
+++ src/main/resources/data/student.sql
@@ -1,12 +1,12 @@
 INSERT INTO STUDENT (student_id, first_name, last_name, email, major, student_year, last_registration_date)
     VALUES
         (1, 'John', 'Doe', 'john.doe@example.com', 'Computer Science', 'Sophomore', '2023-05-15'),
-        (2, 'Jane', 'Smith', 'jane.smith@example.com', 'Engineering', 'Junior', '2023-04-20'),
-        (3, 'Alex', 'Johnson', 'alex.johnson@example.com', 'Mathematics', 'Freshman', '2023-09-10'),
-        (4, 'Emily', 'Brown', 'emily.brown@example.com', 'Biology', 'Senior', '2022-12-01'),
+        (2, 'Jane', 'Smith', 'jane.smith@example.com', 'Art and Design', 'Junior', '2023-04-20'),
+        (3, 'Alex', 'Johnson', 'alex.johnson@example.com', 'Business Administration', 'Freshman', '2023-09-10'),
+        (4, 'Emily', 'Brown', 'emily.brown@example.com', 'Psychology', 'Senior', '2022-12-01'),
         (5, 'Michael', 'Taylor', 'michael.taylor@example.com', 'Business Administration', 'Sophomore', '2023-03-18'),
         (6, 'Olivia', 'Clark', 'olivia.clark@example.com', 'Psychology', 'Junior', '2023-07-25'),
-        (7, 'Daniel', 'Wang', 'daniel.wang@example.com', 'Chemistry', 'Senior', '2022-11-05'),
-        (8, 'Sophia', 'Garcia', 'sophia.garcia@example.com', 'Art History', 'Freshman', '2023-08-30'),
-        (9, 'Ethan', 'Lopez', 'ethan.lopez@example.com', 'Political Science', 'Junior', '2023-02-14'),
-        (10, 'Ava', 'Martinez', 'ava.martinez@example.com', 'English Literature', 'Senior', '2022-10-12');
+        (7, 'Daniel', 'Wang', 'daniel.wang@example.com', 'Computer Science', 'Senior', '2022-11-05'),
+        (8, 'Sophia', 'Garcia', 'sophia.garcia@example.com', 'Art and Design', 'Freshman', '2023-08-30'),
+        (9, 'Ethan', 'Lopez', 'ethan.lopez@example.com', 'Computer Science', 'Junior', '2023-02-14'),
+        (10, 'Ava', 'Martinez', 'ava.martinez@example.com', 'Art and Design', 'Senior', '2022-10-12');
